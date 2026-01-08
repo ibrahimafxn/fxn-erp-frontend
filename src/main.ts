fb9dcdfc066db997fdc961e92893d293866ae2b6
ibrahimafxn <ibrahima.fxn@gmail.com>
2026-01-08 20:43:53 +0100

    feat(pwa): enable offline + mobile polish

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,15 @@ import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 
 import {App} from './app/app';
 import {AppConfig} from './app/app.config';
-import {registerLocaleData} from '@angular/common';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 bootstrapApplication(App, {
   providers: [
     provideRouter(AppConfig.routes),
     provideHttpClient(withInterceptorsFromDi()), // Angular injecte tous les interceptors fournis
-    ...AppConfig.providers,
+    ...AppConfig.providers, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ]
 }).catch(err => console.error(err));
